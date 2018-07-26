2aadd85aaf8ffcc735abe70764e5069856170dd3
elynagh <ethanlynagh@gmail.com>
2018-07-26 17:58:26 -0400

    Create PrizeCard.java

---
+++ GamblingGameThing/PrizeCard.java
@@ -0,0 +1,35 @@
+public class PrizeCard {
+  private int id;
+  private String color;
+  private int prizeAmount;
+  
+  public PrizeCard() {
+    id = 0;
+    color = "red";
+    prizeAmount = 0;
+  }
+  
+  public PrizeCard(int nID, String nColor, int nPrizeAmount) {
+   id = nID;
+   color = nColor;
+   prizeAmount = nPrizeAmount;
+  }
+  
+  public int getID() {
+    return id;
+  }
+
+  public String getColor(){
+    return color;
+  }
+  
+  public int getPrizeAmount() {
+    return prizeAmount;
+  }
+  
+  public String toString() {
+    return "ID: " + id +
+      ", Color: " + color +
+      ", Prize Amount: $" + prizeAmount;      
+  }
+}
